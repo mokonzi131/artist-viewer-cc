98e9febf938430133a430edf490d85260b5cbc4b
Michael Landes <mlandes@logiconix.com>
2017-12-17 20:54:40 -0800

    clean up stuff we don't need from the template

--- ChallengeApplication/WebApp/ClientApp/app/app.shared.module.ts
+++ ChallengeApplication/WebApp/ClientApp/app/app.shared.module.ts
@@ -7,8 +7,6 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './components/app/app.component';
 import { NavMenuComponent } from './components/navmenu/navmenu.component';
 import { HomeComponent } from './components/home/home.component';
-import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
-import { CounterComponent } from './components/counter/counter.component';
 import { ArtistsComponent } from './components/artists/artists.component';
 import { LogoutComponent } from './components/logout/logout.component';
 
@@ -16,8 +14,6 @@ import { LogoutComponent } from './components/logout/logout.component';
     declarations: [
         AppComponent,
         NavMenuComponent,
-        CounterComponent,
-        FetchDataComponent,
         ArtistsComponent,
         LogoutComponent,
         HomeComponent
@@ -29,8 +25,6 @@ import { LogoutComponent } from './components/logout/logout.component';
         RouterModule.forRoot([
             { path: '', redirectTo: 'home', pathMatch: 'full' },
             { path: 'home', component: HomeComponent },
-            { path: 'counter', component: CounterComponent },
-            { path: 'fetch-data', component: FetchDataComponent },
             { path: 'artists', component: ArtistsComponent },
             { path: 'logout', component: LogoutComponent },
             { path: '**', redirectTo: 'home' }
