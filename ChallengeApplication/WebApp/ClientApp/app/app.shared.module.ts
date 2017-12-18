5364bab110083c91c9e637f7bcea134c580dbb55
Michael Landes <mlandes@logiconix.com>
2017-12-17 19:17:03 -0800

    Enable viewing artists in a section of the web app

--- ChallengeApplication/WebApp/ClientApp/app/app.shared.module.ts
+++ ChallengeApplication/WebApp/ClientApp/app/app.shared.module.ts
@@ -9,6 +9,7 @@ import { NavMenuComponent } from './components/navmenu/navmenu.component';
 import { HomeComponent } from './components/home/home.component';
 import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
 import { CounterComponent } from './components/counter/counter.component';
+import { ArtistsComponent } from './components/artists/artists.component';
 
 @NgModule({
     declarations: [
@@ -16,6 +17,7 @@ import { CounterComponent } from './components/counter/counter.component';
         NavMenuComponent,
         CounterComponent,
         FetchDataComponent,
+        ArtistsComponent,
         HomeComponent
     ],
     imports: [
@@ -27,6 +29,8 @@ import { CounterComponent } from './components/counter/counter.component';
             { path: 'home', component: HomeComponent },
             { path: 'counter', component: CounterComponent },
             { path: 'fetch-data', component: FetchDataComponent },
+            { path: 'artists', component: ArtistsComponent },
+            { path: 'logout', component: FetchDataComponent },
             { path: '**', redirectTo: 'home' }
         ])
     ]
---
+++ ChallengeApplication/WebApp/ClientApp/app/components/artists/artists.component.ts
@@ -0,0 +1,22 @@
+﻿import { Component, Inject } from '@angular/core';
+import { Http } from '@angular/http';
+
+@Component({
+    templateUrl: './artists.component.html'
+})
+export class ArtistsComponent {
+    public artists: ArtistData[];
+
+    constructor(http: Http, @Inject('BASE_URL') baseUrl: string) {
+        http.get(baseUrl + 'api/artists').subscribe(result => {
+            this.artists = result.json() as ArtistData[];
+        }, error => console.error(error));
+    }
+}
+
+interface ArtistData {
+    id: string;
+    name: string;
+    imageUrl: string;
+    artistUrl: string;
+}
